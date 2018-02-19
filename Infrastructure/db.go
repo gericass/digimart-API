fbcd97b23394f2b5977be7900096f72a610186ca
gericass <bonjovi1926@yahoo.co.jp>
2018-02-20 02:11:56 +0900

    add: DDL

--- Infrastructure/db.go
+++ Infrastructure/db.go
@@ -3,16 +3,15 @@ package Infrastructure
 import "database/sql"
 
 func TXHandler(db sql.DB, f func(tx sql.Tx)) error {
-
+	return nil
 }
 
 func (inst *Instrument) Insert(db sql.DB) error {
 	var err error
 	f := func(tx sql.Tx) {
-		tx.Exec("INSERT INTO instruments()")
+		tx.Exec("INSERT INTO instruments(test,test2) VALUES (?,?)", 12, 12)
 	}
-
-
+	err = TXHandler(db,f)
 	if err != nil {
 		return err
 	}
