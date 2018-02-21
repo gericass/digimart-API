7f4d9d235079ff15566c3025723edacf0cc51400
gericass <bonjovi1926@yahoo.co.jp>
2018-02-21 23:53:33 +0900

    add: insert implement

--- Infrastructure/db.go
+++ Infrastructure/db.go
@@ -2,16 +2,33 @@ package Infrastructure
 
 import "database/sql"
 
-func TXHandler(db sql.DB, f func(tx sql.Tx)) error {
-	return nil
+func TXHandler(tx *sql.Tx, f func(tx *sql.Tx) error) error {
+	var err error
+	err = f(tx)
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+	return err
 }
 
 func (inst *Instrument) Insert(db sql.DB) error {
-	var err error
-	f := func(tx sql.Tx) {
-		tx.Exec("INSERT INTO instruments(test,test2) VALUES (?,?)", 12, 12)
+	tx, err := db.Begin()
+	if err != nil {
+		return err
 	}
-	err = TXHandler(db,f)
+	f := func(tx *sql.Tx) error {
+		// TODO
+		_, err := tx.Exec("INSERT INTO instruments(test,test2) VALUES (?,?)", 12, 12)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	err = TXHandler(tx, f)
 	if err != nil {
 		return err
 	}
