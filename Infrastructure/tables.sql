837ae47f14e6be1dd1412fcd367af0efcc384989
gericass <bonjovi1926@yahoo.co.jp>
2018-02-25 23:14:24 +0900

    fix: ddl

---
+++ Infrastructure/tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE instruments (
+  `id`            BIGINT UNSIGNED       AUTO_INCREMENT PRIMARY KEY,
+  `name`          VARCHAR(256) NOT NULL,
+  `category`      VARCHAR(64)  NOT NULL,
+  `price`         INT          NOT NULL,
+  `condition`     VARCHAR(64)  NOT NULL,
+  `status`        BOOL         NOT NULL,
+  `url`           TEXT         NOT NULL,
+  `register_date` DATETIME     NOT NULL,
+  `created_at`    TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE users (
+  id         BIGINT UNSIGNED      AUTO_INCREMENT PRIMARY KEY,
+  name       VARCHAR(26) NOT NULL,
+  password   VARCHAR(26) NOT NULL,
+  salt       VARCHAR(6)  NOT NULL,
+  created_at TIMESTAMP   NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP   NOT NULL DEFAULT NOW()
+);
+
+-- instrumentsとusersの中間テーブル
+CREATE TABLE subscribes (
+  id            BIGINT UNSIGNED          AUTO_INCREMENT PRIMARY KEY,
+  user_id       BIGINT UNSIGNED NOT NULL,
+  instrument_is BIGINT UNSIGNED NOT NULL,
+  created_at    TIMESTAMP       NOT NULL DEFAULT NOW()
+);
