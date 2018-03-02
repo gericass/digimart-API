f355a5a4d4f26abd7ef7d38c3dff853eff3830db
gericass <bonjovi1926@yahoo.co.jp>
2018-03-02 16:01:21 +0900

    result.html

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,6 +1,12 @@
 package scraper
 
+import "github.com/PuerkitoBio/goquery"
+
 func NewArrival() ([]*Instrument, error) {
+	doc, err := goquery.NewDocument("https://www.digimart.net")
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
